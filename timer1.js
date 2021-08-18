11f6a8989422f0dc5c6a8a7b04360ba56a7c559b
Lloyd Worth <lloydrww@gmail.com>
2021-08-18 19:37:45 +0000

    code review and refactored with Lovemore

--- timer1.js
+++ timer1.js
@@ -1,34 +1,17 @@
+const array = process.argv.slice(2);
+const beep = function(delay) {
+  setTimeout(() => { // our time out
+    process.stdout.write('beep\n')   // types the output in a single line
+  }, delay * 1000)
+}
 
-  for (let i = 2; i < process.argv.length; i++) {
-    if(!process.argv === Number || process.argv <+ 0 || process.argv === "" ) {
-      return;
-    }
-    let delay = i * process.argv.length
-    setTimeout(() => { // our time out
-  console.log('c')   // types the output in a single line
-  }, delay) 
+for (element of array) {
+  if(!isNaN(element) && element > 0 && element !== "" ) {      
+      beep(element)    
+  }  
 
-  }
-
-
-
-
-
-// // let input = process.argv;
-
-// // for (let i = 0; i < input.length; i++) {
-// //  console.log('\x07')
-  
-// // }
-
-// //   for (let i = 0; i < process.argv.length; i++) {
-// //     let delay = i * process.argv
-// //     setTimeout(() => { // our time out
-// //    process.stdout.write('\x07')    // types the output in a single line
-// //   }, delay) 
-// // }
+}
 
 
-// setTimeout(() => {
-//   console.log('hello')
-// }, process.argv)
+
+
